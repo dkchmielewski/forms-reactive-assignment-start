0dbb3755b0e3b5c4cdf907af60f46352dd2893d3
DAN-PC\Daniel <dkchmielewski@gmail.com>
2022-10-06 23:58:09 -0400

    added validator and async validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { CustomValidators } from './custom-validators'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,7 +12,11 @@ export class AppComponent {
 
   ngOnInit() {
     this.projectForm = new FormGroup({
-      'projectName': new FormControl(null, Validators.required),
+      'projectName': new FormControl(
+        null, 
+        [Validators.required, CustomValidators.invalidProjectName],
+        CustomValidators.asyncInvalidProjectName
+      ),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'projectStatus': new FormControl('critical')
     });
